34fd41801bc826c5c7f4c913da936bb2b16075f1
oskar.potaczek <oskar.potaczek@wfirma.pl>
2024-04-26 08:57:17 +0200

    add lazy.nvim

---
+++ lua/user/options.lua
@@ -0,0 +1,13 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.showmode = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
---
+++ lua/user/plugins.lua
@@ -0,0 +1,100 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup {}
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		build = "make install_jsregexp",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require('cmp')
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require('luasnip').lsp_expand(args.body)
+					end,
+				},
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' },
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+
+			-- Set configuration for specific filetype
+			cmp.setup.filetype('gitcommit', {
+				sources = cmp.config.sources({
+					{ name = 'git' },
+				}, {
+					{ name = 'buffer' },
+				})
+			})
+
+			-- Use buffer source for '/' and '?'
+			cmp.setup.cmdline({ '/', '?' }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = 'buffer' },
+				}
+			})
+
+			-- Use cmdline & path source for ':'
+			cmp.setup.cmdline(':', {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = 'path' },
+				}, {
+					{ name = 'cmdline' }
+				})
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				height = 0.8,
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			automatic_installation = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local lsp = require 'lspconfig'
+
+			lsp.intelephense.setup({
+				capabilities = capabilities
+			})
+			lsp.tsserver.setup({})
+		end
+	}
+}
