b7212949d692d309760007a7bfa8a18e471e03df
oskar.potaczek <oskar.potaczek@wfirma.pl>
2024-07-10 11:47:05 +0200

    remove ranger.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -109,17 +109,5 @@ return {
 			lsp.tsserver.setup({})
             lsp.basedpyright.setup({})
 		end
-	},
-        {
-            "kelly-lin/ranger.nvim",
-            config = function()
-                require("ranger-nvim").setup({ replace_netrw = true })
-                vim.api.nvim_set_keymap("n", "<leader>ef", "", {
-                    noremap = true,
-                    callback = function()
-                        require("ranger-nvim").open(true)
-                    end,
-                })
-            end,
-        }
+	}
 }
