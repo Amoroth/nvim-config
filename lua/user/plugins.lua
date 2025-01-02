a774b3d2597822f17a23fde4554d67fd9558fd53
oskar.potaczek <oskar.potaczek@wfirma.pl>
2025-01-02 14:08:41 +0100

    add telescope keymaps

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,7 +23,15 @@ return {
     "nvim-lua/plenary.nvim",
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function ()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+
+
+        end
     },
     {
         "nvim-lualine/lualine.nvim",
