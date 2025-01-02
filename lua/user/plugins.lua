06304aa11b106505b0e87ce0a080b860df85f80f
oskar.potaczek <oskar.potaczek@wfirma.pl>
2025-01-02 13:16:19 +0100

    setup lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -28,6 +28,14 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local lualine = require('lualine')
+            lualine.setup({
+                sections = {
+                    lualine_x = {'filetype'}
+                }
+            })
+        end
     },
     {
         'stevearc/oil.nvim',
